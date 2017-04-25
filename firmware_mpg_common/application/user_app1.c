cf5d6e980dbca121caa5435616a4d16ac9f64e16
PengHuizdh <289886693@qq.com>
2017-04-25 15:38:17 +0800

    Homework second code
    Breathe light, but it have obvious flicker

--- firmware_mpg_common/application/user_app1.c
+++ firmware_mpg_common/application/user_app1.c
@@ -136,131 +136,47 @@ State Machine Function Definitions
 /* Wait for ??? */
 static void UserApp1SM_Idle(void)
 {
-     u8 u8Stop = 0;
-     static u32 u32Cycle = 0;
+     static u8 u8Stop = 0;
+     static u8 u8i = 0;
      static u32 u32Counter = 0;
-     static bool bLightOn = 0;
      static u32 u32Counter1 = 0;
-     u32 au32Time[11] = {500,250,125,68,34,15,34,68,125,250,500};
-     u32Counter++;
-     u32Counter1++;
-/* When time to 2s, let the number in the array point to the next one*/     
-      if(u32Counter1/2000 == 1)
-      {
-        u32Cycle++;
-        u32Counter1 = 0;
-        u32Counter = 0;
-      }
-/*Let the flashing time be the time I want, this time in the array*/  
-      if(u32Counter == au32Time[u32Cycle])
-      {
-        u8Stop = 1;
-      }
-      
-      
-    while(u8Stop)
-    {
-          
+     static u32 u32Counter2 = 0;
+     u32 au32Light[11] = {0,1,2,3,4,5,6,7,8,9,10};
      
-           if(bLightOn)
-             {
-                 HEARTBEAT_OFF();
-             }
-             else
-             {
-                 HEARTBEAT_ON();
-             }
-             bLightOn = !bLightOn;
-             u32Counter = 0;
-             if(u32Cycle == 10)
-              {
-                  u32Cycle = 0;
-              }
-             u8Stop = 0;
-                                   
-
-    }
-    /*This code is conck by Peng Hui*/
-    
-    
-    
- /* static u32 u32CounterLimit=480; 
-  static u32 u32Counter1=0,u32Counter2=0,u32LoopCount=0;
-  static bool bLightOn=FALSE;
-  u32Counter1++;
-  u32Counter2++;
-  if((u32LoopCount / 1920) >=5)
-  {
-    u32LoopCount++;
-       if((u32LoopCount/1920) == 11)
-          {
-             u32LoopCount = 0;
-             u32Counter1 = 0;   
-          }
-      if(u32Counter2==u32CounterLimit)
-       {   
-         u32Counter2=0;
-         
-         if(bLightOn)
-          {
-            HEARTBEAT_OFF();
-          }
-         else
-          {
-            HEARTBEAT_ON();
-           }
-            bLightOn=!bLightOn;
-           
-
-         
-        }
-      if(u32Counter1 == 1920)
-      {
+     u32Counter++;
+     u32Counter1++; 
+     if(u32Counter2 > au32Light[u8i] + 1)
+     {
+       HEARTBEAT_OFF();
+       u32Counter2 = 0;
        
-      
-        u32CounterLimit=u32CounterLimit*2;
-        
-         u32Counter1=0;
-         
-      }
-           
-        
-   
-    
-  }
-  else
-  {
-    
-    
-    
-       u32LoopCount++;
-   
-      if(u32Counter2==u32CounterLimit)
-       {   
-         u32Counter2=0;
-         if(bLightOn)
-          {
-            HEARTBEAT_OFF();
-          }
-         else
-          {
-            HEARTBEAT_ON();
-           }
-            bLightOn=!bLightOn;
-      
-         
+     } 
+     if(u32Counter == 100)
+     {
+        u8i++;
+        u32Counter = 0;
+     }
+     if(u8i == 11)
+     {
+        u8i = 0;
+     }
+      if(u32Counter1 == 10)
+        {
+          u8Stop = 1;
+          u32Counter2++;
         }
-       if(u32Counter1==1920)
-    {
-        u32CounterLimit=u32CounterLimit/2;
-        u32Counter1=0;
-    }
-    
-  }*/
+     
+       while(u8Stop)
+       {
+          if(u32Counter2 == au32Light[u8i] + 1)
+          {
+             HEARTBEAT_ON();
+          }
 
-  /*This code is conck by Luo Chuhan*/
-        
-
+          u32Counter1 = 0;
+          u8Stop = 0;
+       }
+     
     
   
 } /* end UserApp1SM_Idle() */
