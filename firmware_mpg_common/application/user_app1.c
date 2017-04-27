06a71cff4aedfc999207531d09dbf52d5e3b7c7a
PengHuizdh <289886693@qq.com>
2017-04-27 12:52:27 +0800

    Luo Chuhan's code

--- firmware_mpg_common/application/user_app1.c
+++ firmware_mpg_common/application/user_app1.c
@@ -136,7 +136,42 @@ State Machine Function Definitions
 /* Wait for ??? */
 static void UserApp1SM_Idle(void)
 {
-  
+  u32 au32OnTime[ ]={0,1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1,0};
+static u32 u32RankNumber=0;
+static u32 u32LoopCount=0;
+static u32 u32OnTimeCount=0;
+
+
+
+u32OnTimeCount++;
+ u32LoopCount++;
+ if(u32OnTimeCount<au32OnTime[u32RankNumber]+1)
+ {
+   HEARTBEAT_ON();
+ }
+ else
+ {
+   HEARTBEAT_OFF();
+ }
+ 
+ if(u32OnTimeCount==10)
+ {
+   u32OnTimeCount=0;
+ }
+ 
+ if(u32LoopCount==100)
+ {
+   if(u32RankNumber==20)
+   {
+     u32RankNumber=0;
+     u32LoopCount=0;
+   }
+   else
+   {
+     u32RankNumber++;
+     u32LoopCount=0;
+   }
+ }
      
     
   
